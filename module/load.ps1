8d4e31b307e11d51e1e1a94ced6cb1680cf67733
niels vanhaecke <niels@niels>
2020-09-03 18:07:40 +0200

    added load script

---
+++ module/load.ps1
@@ -0,0 +1,6 @@
+import-module .\module\nlvm
+import-module .\module\nlhomedir
+import-module .\module\nlhomedirpermissons
+import-module .\module\nlOUstructer
+import-module .\module\nlsharepermissions
+import-module .\module\nlgrouppermissions
