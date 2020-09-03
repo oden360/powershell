f349da07d04e77cba44af66a8daab27f9e3a5324
oden360 <63650312+oden360@users.noreply.github.com>
2020-09-03 14:55:36 +0200

    Add files via upload
    ca

---
+++ module/nladd-ipsecrule.ps1
@@ -0,0 +1,14 @@
+﻿$rulename="ttu"
+$locala="192.168.0.0/16"
+$remotea="192.168.0.0/16"
+$authority="DC=den, DC=niels, CN=niels-CA-CA"
+#$authority="ca.niels.den\niels-CA-CA"
+$methode = 1
+
+$qMProposal = New-NetIPsecQuickModeCryptoProposal -Encapsulation ESP -ESPHash SHA1 -Encryption DES3 
+$qMCryptoSet = New-NetIPsecQuickModeCryptoSet -DisplayName "esp:sha1-des3"      -Proposal $qMProposal 
+# = ca =
+$NewCertProposal = New-NetIPsecAuthProposal -Machine -Cert -Authority $authority   -AuthorityType root 
+# = kerberos =
+#$NewCertProposal = New-NetIPsecAuthProposal -Machine -Kerberos 
+New-NetIPSecRule -DisplayName $rulename -Mode Tunnel -LocalAddress $locala -RemoteAddress $remotea -InboundSecurity Require -OutboundSecurity Require -QuickModeCryptoSet $qMCryptoSet.Name -Phase1AuthSet $NewCertProposal
